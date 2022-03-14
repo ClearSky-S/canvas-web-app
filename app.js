d503d636b4c7340afa7b75943fa6db065a3b07e7
2021038131 <ggogono2@gmail.com>
2022-03-14 14:02:18 +0900

    color

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const canvas = document.getElementById('jsCanvas');
-
-let painting = false
+// canvas.width = 1920;
+// canvas.height = 1080;
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = "#";
+ctx.lineWidth = 0.5;
+const colors = document.querySelectorAll('#jsColors button');
+console.log(colors);
+let painting = false;
 
 
 if(canvas){
     canvas.addEventListener('mousemove', (event)=>{
-        const x = event.offsetX;
-        const y = event.offsetY;
-        console.log(x,y,painting);
+        const x = event.offsetX/canvas.offsetWidth*canvas.width;
+        const y = event.offsetY/canvas.offsetHeight*canvas.height;
+        // console.log(x,y);;
+        if(!painting){
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
     })
     canvas.addEventListener('mousedown',(event)=>{
         painting = true;
@@ -18,4 +31,14 @@ if(canvas){
     canvas.addEventListener('mouseleave',(event)=>{
         painting = false;
     })
+
+    document.getElementById('jsClear').addEventListener('click',(event)=>{
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    })
 }
+
+colors.forEach((color)=>{
+    color.addEventListener('click',(event)=>{
+        ctx.strokeStyle = event.target.style.backgroundColor;
+    })
+});
